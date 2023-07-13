f45f02f42f1af7e003663b66f7d338e8c639cf3d
akinbye <akinbye3@gmail.com>
2023-07-13 11:54:10 -0700

    gcc 6-size.c -m64 -o size64 2> /tmp/64

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,7 +11,7 @@ int main(void)
 	long long int d;
 	float e;
 printf("Size of a char: %lu byte(s)\n", sizeof(a));
-printf("size of an int: %lu byte(s)\n", sizeof(b));
+printf("Size of an int: %lu byte(s)\n", sizeof(b));
 printf("Size of a long int: %lu byte(s)\n", sizeof(c));
 printf("Size of a long long int: %lu byte(s)\n", sizeof(d));
 printf("Size of a float: %lu byte(s)\n", sizeof(e));
