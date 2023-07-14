a612f8a4a18b446038692f77a07a2586413c6e54
akinbye <akinbye3@gmail.com>
2023-07-14 09:31:55 -0700

    5-print_numbers.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,19 +6,19 @@
  **/
 int main(void)
 {
-	int n = 97;
-	int i = 65;
-	
-	while (n <= 122)
-	{
-		putchar(n);
-		n++;
-	}
-	while (i <= 90)
-	{
-		putchar(i);
-		i++;
-	}
+int n = 97;
+int i = 65;
+
+while (n <= 122)
+{
+	putchar(n);
+	n++;
+}
+while (i <= 90)
+{
+	putchar(i);
+	i++;
+}
 	putchar('\n');
 	return (0);
 }
