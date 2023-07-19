2f8fff7794062d052cf7b2386bc98b67b6100f93
akinbye <akinbye3@gmail.com>
2023-07-18 18:47:25 -0700

    new

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -19,18 +19,24 @@ void print_to_98(int n)
 			}
 			else
 			{
-				printf("%d,", n);
+				printf("%d, ", n);
 			}
-		}
-		for (; n >= 98; n--)
-		{
-			printf("%d", n);
-			printf("\n");
-			break;
 		}
 	}
 	else
 	{
-		printf("%d,", n);
+		for (; n >= 98; n--)
+		{
+			if (n == 98)
+			{
+				printf("%d", n);
+				printf("\n");
+				break;
+			}
+			else
+			{
+				printf("%d,", n);
+			}
+		}
 	}
 }
