ac356210357046a12f4547605acea2dce7a24780
akinbye <akinbye3@gmail.com>
2023-07-24 06:37:15 -0700

    1-swap.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,7 +3,7 @@
  *reset_to_98 - A function that takes a pointer to an int as parameter and
  *updates the value it points to to 98
  *@n: integer
- *Return: 0
+ *Return: n
  */
 void reset_to_98(int *n);
 {
--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -3,7 +3,6 @@
  *swap_int - A function that swaps the values of two integers
  *@a: integer
  *@b: integer
- *Return: 0
  */
 void swap_int(int *a, int *b);
 {
