f587309e36e6623855c83394d1b6075974aa95d2
akinbye <akinbye3@gmail.com>
2023-08-04 06:33:47 -0700

    3-puts.o

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,5 +1,13 @@
 #include "main.h"
-#include <stdio.h>
-
 void _puts(char *str);
-int main(void);
+
+#include <stdio.h>
+/**
+ *main - entry point
+ *Return: 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
