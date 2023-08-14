b68f7b453fd7f94ec15837a0d1e31981fc8cbb5a
akinbye <akinbye3@gmail.com>
2023-08-14 06:55:10 -0700

    5-free_dog.c

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -3,10 +3,13 @@
 /**
  *free_dog - a function that bfrees dogs
  *@d: pointer to dog_t
- *Return: 0
  */
 void free_dog(dog_t *d)
 {
 	while (d)
+	{
 		free(d);
+		free(d->name);
+		free(d->owner);
+	}
 }
